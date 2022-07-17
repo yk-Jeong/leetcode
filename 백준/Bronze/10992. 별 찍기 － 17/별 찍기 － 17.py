0b8e1fab3e009ffb85da6ee4267d577b194d13f9
Jeong YK/Ellie <90163856+yk-Jeong@users.noreply.github.com>
2022-07-17 20:47:56 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
@@ -1,9 +1,8 @@
 n = int(input())
 for i in range(1, n+1):
-  if i != n: 
-    if i == 1: 
-      print(" "*(n-i)+"*"+" ")
-    else:
-      print(" "*(n-i)+"*"+" "*(2*i - 3) +"*")
-  else:    
+  if i == 1:
+    print(" "*(n-i)+"*"+" ")
+  elif i != n:
+     print(" "*(n-i)+"*"+" "*(2*i - 3) +"*")
+  else:
     print((2*i - 1)*"*")
