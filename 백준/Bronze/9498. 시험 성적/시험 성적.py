7d835662f3dd79df0da592ac21b6fb980acf3673
Jeong YK/Ellie <90163856+yk-Jeong@users.noreply.github.com>
2022-07-23 16:39:23 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+test = int(input())
+if test <= 100 and test >= 90:
+  print("A")
+elif test < 90 and test >= 80:
+  print("B")
+elif test < 80 and test >= 70:
+  print("C")
+elif test < 70 and test >= 60:
+  print("D")
+else: 
+  print("F")
