c8256cacecf544db231c788b43a7a198873e39ca
Jeong YK/Ellie <90163856+yk-Jeong@users.noreply.github.com>
2022-07-31 00:46:35 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,15 @@
-N = int(input()) # 입력받을 숫자의 개수
-nums = list(map(int, input().split(' '))) # 공백으로 숫자 구분. ex) 1 3 5 7
-resCnt = 0 # 소수의 개수
+N = int(input())
+list_n = list(map(int, input().split()))
 
-for i in nums:
-    cnt = 0 
-    if(i == 1): # 1은 소수가 아니기 때문에 건너띔
-        continue
-    for j in range(2, i+1):
-        if(i % j == 0):
-            cnt += 1
-    if(cnt == 1):
-        resCnt += 1
-print(resCnt)
+prime = 0
+for i in list_n:
+  count = 0
+  if i == 1:
+    continue
+  for j in range(2, i+1):
+    if i % j == 0:
+      count += 1
+  if count == 1:
+    prime += 1
+
+print(prime)
