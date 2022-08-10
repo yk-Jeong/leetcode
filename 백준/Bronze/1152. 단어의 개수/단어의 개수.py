4724d2b210797e11674d61d9385b3b3502bc89c0
Jeong YK/Ellie <90163856+yk-Jeong@users.noreply.github.com>
2022-08-10 21:58:31 +0900

    [Bronze II] Title: 단어의 개수, Time: 80 ms, Memory: 38416 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,8 +1,5 @@
-a = input()
-count = 0
-count_list = a.split(' ')
-for i in range(len(count_list)):
-  if len(count_list[i]) > 0:
-    count += 1
-
-print(count)
+a = input().strip()
+if len(a) == 0:
+  print("0")
+else: 
+  print(len(a.split(' ')))
