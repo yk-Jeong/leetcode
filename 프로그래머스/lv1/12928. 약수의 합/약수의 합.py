98ec1cb80c25c85c5fbab247e32d3b59c918dcd6
유경 <90163856+yk-Jeong@users.noreply.github.com>
2023-02-07 14:57:37 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,13 @@
+def solution(n):
+    
+    answer = []
+
+    for i in range(1, n+1):
+        if (n % i) != 0: 
+            pass
+        else:
+            answer.append(i)
+
+    answer = sum(set(answer))
+    
+    return answer
